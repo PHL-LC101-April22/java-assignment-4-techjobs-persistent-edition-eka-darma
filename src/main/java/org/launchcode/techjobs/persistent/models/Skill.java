9678169a439299a260646da9351da78bc3387ed0
Eka Darmawan <eka_darma@hotmail.com>
2022-10-01 11:47:27 -0400

    Solved Task 1 and 1/2 Task 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @Size(max=200)
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
