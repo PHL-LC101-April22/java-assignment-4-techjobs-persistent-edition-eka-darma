730fa6b7a8f8f919f72f81bdf0bc93e0cd5cc538
Eka Darmawan <eka_darma@hotmail.com>
2022-10-06 17:18:22 -0400

    Solved Task Three

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,23 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+import org.hibernate.mapping.ToOne;
+
 import javax.persistence.*;
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity {
 
-    @Id
-    @GeneratedValue
-    private int id;
+    @ManyToOne
+    private Employer employer;
 
-    private String name;
-
-    private String employer;
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//
+//    private String name;
+//
+//    private String employer;
     private String skills;
+
+
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer anEmployer, String someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
@@ -25,22 +32,31 @@ public class Job{
 
     // Getters and setters.
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getEmployer() {
+//        return employer;
+//    }
+//
+//    public void setEmployer(String employer) {
+//        this.employer = employer;
+//    }
+//
     public String getSkills() {
         return skills;
     }
@@ -48,4 +64,6 @@ public class Job{
     public void setSkills(String skills) {
         this.skills = skills;
     }
+
+
 }
